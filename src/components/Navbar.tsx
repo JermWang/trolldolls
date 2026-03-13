f77105c275cf1e93065dbf05c4fb3729290fd705
JermWang <jermsobased@gmail.com>
2026-03-13 09:03:08 -0700

    Add X community icon link to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -20,6 +20,17 @@ export default function Navbar() {
           <a href="#gallery" className="hover:text-purple transition-colors hover:-rotate-2 inline-block">
             troll army
           </a>
+          <a
+            href="https://x.com/i/communities/2032483917899448654/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-pink transition-colors hover:scale-110 inline-block"
+            title="Join our X Community"
+          >
+            <svg viewBox="0 0 24 24" className="w-5 h-5 fill-current" aria-hidden="true">
+              <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
+            </svg>
+          </a>
         </div>
 
         <a
